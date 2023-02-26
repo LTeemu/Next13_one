d5da2d5b2936a25b974c6d162d6a20df46c70890
LTeemu <Teemu.Leinonen2@gmail.com>
2023-02-26 22:38:44 +0200

    -

--- app/animals/page.jsx
+++ app/animals/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 import Image from 'next/image'
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 import { gsap, Expo } from 'gsap'
 import pic1 from '/public/images/owl.webp'
 import pic2 from '/public/images/bird.webp'
@@ -8,14 +8,11 @@ import pic3 from '/public/images/crab.webp'
 import pic4 from '/public/images/rabbit.webp'
 import pic5 from '/public/images/panda.webp'
 
-export default function Animals() {
-  const [active, setActive] = useState()
-  const imgRef = useRef()
-  const imgTL = useRef()
-
-  const defaultImg = { src: pic1, mask: 'url(/images/mask1.webp)' }
+export default function Animals2() {
+  const [activeIndex, setActiveIndex] = useState(0)
 
   const images = [
+    { src: pic1, mask: 'url(/images/mask1.webp)' }, //default
     { src: pic2, mask: 'url(/images/mask2.webp)', text: '1. Hover, tab and touch' },
     { src: pic3, mask: 'url(/images/mask3.webp)', text: '2. Cool images' },
     { src: pic4, mask: 'url(/images/mask4.webp)', text: '3. GSAP animation' },
@@ -23,56 +20,58 @@ export default function Animals() {
   ]
 
   useEffect(() => {
-    imgTL.current = gsap.timeline({ defaults: { duration: 2, ease: Expo.easeOut } })
-    imgTL.current.fromTo(imgRef.current, { scale: 1.1 }, { scale: 1 })
-  }, [])
-
-  useEffect(() => {
-    imgTL.current.play(0)
-  }, [active])
+    gsap.fromTo(`img[data-animal="${activeIndex}"]`, { scale: 1.1 }, { scale: 1, duration: 2, ease: Expo.easeOut })
+  }, [activeIndex])
 
   return (
     <>
       <div className='relative grid max-w-5xl grid-cols-3 mx-auto'>
-        <div className='flex flex-col h-full justify-evenly'>
-          {images.map((item, i) => {
+        <div className='flex flex-col col-span-1'>
+          {images.slice(1).map((item, i) => {
             return (
               <span
                 key={i}
                 tabIndex={0}
-                className='flex items-center h-full col-span-1 px-4 py-4'
-                onTouchStartCapture={() => setActive(i)}
-                onTouchCancelCapture={() => setActive()}
-                onFocusCapture={() => setActive(i)}
-                onBlurCapture={() => setActive()}
-                onMouseEnter={() => setActive(i)}
-                onMouseLeave={() => setActive()}
+                className='flex items-center h-full px-4 border border-slate-50'
+                onTouchStartCapture={() => setActiveIndex(i + 1)}
+                onTouchCancelCapture={() => setActiveIndex(0)}
+                onFocusCapture={() => setActiveIndex(i + 1)}
+                onBlurCapture={() => setActiveIndex(0)}
+                onMouseEnter={() => setActiveIndex(i + 1)}
+                onMouseLeave={() => setActiveIndex(0)}
               >
                 {item.text}
               </span>
             )
           })}
         </div>
-        {/*Image + mask container*/}
-        <div
-          style={{
-            WebkitMaskImage: images[active] ? images[active].mask : defaultImg.mask,
-            WebkitMaskPosition: 'center',
-            WebkitMaskRepeat: 'no-repeat',
-            WebkitMaskSize: '100% 100%',
-          }}
-          className='relative col-span-2 max-h-[80vmin] aspect-square mask m-4'
-        >
-          <Image
-            ref={imgRef}
-            src={images[active] ? images[active].src : defaultImg.src}
-            alt=''
-            sizes='66vw'
-            fill
-            priority
-            placeholder='blur'
-            className='object-cover'
-          />
+
+        <div className='relative col-span-2 max-h-[80vmin] aspect-square'>
+          {images.map((img, i) => {
+            return (
+              <div
+                key={i}
+                className='absolute w-full h-full'
+                style={{
+                  WebkitMaskImage: img.mask,
+                  WebkitMaskPosition: 'center',
+                  WebkitMaskRepeat: 'no-repeat',
+                  WebkitMaskSize: '100% 100%',
+                }}
+              >
+                <Image
+                  data-animal={i}
+                  src={img.src}
+                  alt=''
+                  sizes='66vw'
+                  fill
+                  priority
+                  placeholder='blur'
+                  className={`object-cover ${activeIndex === i ? 'visible' : 'invisible'}`}
+                />
+              </div>
+            )
+          })}
         </div>
       </div>
     </>
