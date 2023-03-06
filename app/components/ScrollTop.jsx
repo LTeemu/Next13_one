00426afcfd307791d302dd6c326e2bc6fe5da571
LTeemu <Teemu.Leinonen2@gmail.com>
2023-03-06 20:30:42 +0200

    ScrollTop resize listener

--- app/components/ScrollTop.jsx
+++ app/components/ScrollTop.jsx
@@ -1,16 +1,30 @@
 'use client'
 import { Button } from './Button'
 import { IoMdRocket } from 'react-icons/io'
-import { useState, useLayoutEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { usePathname } from 'next/navigation'
 var _ = require('lodash')
 
 export default function ScrollTop({ classes }) {
   const [scrollVisible, setScrollVisible] = useState(false)
   const pathName = usePathname()
+  const bodyRef = useRef()
 
-  useLayoutEffect(() => {
-    setScrollVisible(document.body.scrollHeight > window.innerHeight * 1.2) //if over 120vh
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      bodyRef.current = document.body
+      const resizeObserver = new ResizeObserver(
+        _.debounce(() => {
+          setScrollVisible(bodyRef.current.scrollHeight > window.innerHeight * 1.1) //Visible if over 110vh
+        }, 100)
+      )
+      resizeObserver.observe(bodyRef.current)
+      return () => resizeObserver.disconnect()
+    }
+  }, [])
+
+  useEffect(() => {
+    bodyRef.current && setScrollVisible(bodyRef.current > window.innerHeight * 1.1)
   }, [pathName])
 
   return (
